b74d71b2f12ba6b90752e0a2e40ea4c63b9748c8
Ryan Dancy <ryan@keal.ca>
2017-04-19 07:13:28 -0400

    Add plugboard in GUI
    It doesn't activate yet, but it will!

--- guiapp.py
+++ guiapp.py
@@ -9,14 +9,29 @@ import kivy
 kivy.require('1.9.1')
 
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import DictProperty
 
 import emulator as em
+
+class Plugboard(Widget):
+  
+  swaps = DictProperty({
+    'A': 'A', 'B': 'B', 'C': 'C', 'D': 'D', 'E': 'E', 'F': 'F', 'G': 'G',
+    'H': 'H', 'I': 'I', 'J': 'J', 'K': 'K', 'L': 'L', 'M': 'M', 'N': 'N',
+    'O': 'O', 'P': 'P', 'Q': 'Q', 'R': 'R', 'S': 'S', 'T': 'T', 'U': 'U',
+    'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'
+  })
+  
+  def activate(self, char):
+    pass # TODO
 
 class EmulatorGui(BoxLayout):
   pass
 
 class EmulatorApp(App):
+  
   def build(self):
     return EmulatorGui()
 
