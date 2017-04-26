d8eae876d0f52759263ebb5d20514b29ab10b213
Ryan Dancy <ryan@keal.ca>
2017-04-26 16:17:38 -0400

    Fix plugboard being weird when moved
    It's not now

--- guiapp.py
+++ guiapp.py
@@ -66,6 +66,9 @@ class Plugboard(Widget):
     'V': 'V', 'W': 'W', 'X': 'X', 'Y': 'Y', 'Z': 'Z'
   })
   
+  char_in = StringProperty('')
+  char_out = StringProperty('')
+  
   def __init__(self, *args, **kwargs):
     super().__init__(*args, **kwargs)
     enigma.set_callbacks(
@@ -73,39 +76,11 @@ class Plugboard(Widget):
   
   def callback_in(self, char_in, char_out, swaps):
     self.swaps.update(swaps)
-    self.activate(char_in, in_=True)
+    self.char_in = char_in
   
   def callback_out(self, char_in, char_out, swaps):
     self.swaps.update(swaps)
-    self.activate(char_out, in_=False)
-  
-  def activate(self, char, in_=True): # in_ is whether the signal is in or out
-    # Find bottom label to draw on
-    label = self.ids['pb%s_btm' % char]
-    
-    # Draw an arrow below it
-    with label.canvas:
-      if in_:
-        Color(0, 1, 0)
-      else:
-        Color(1, 0, 0)
-      
-      triangle_x = label.center_x
-      triangle_y = label.center_y - 6
-      
-      Triangle(points=[triangle_x, triangle_y,
-                       triangle_x - 4, triangle_y - 6,
-                       triangle_x + 4, triangle_y - 6])
-      
-      rect_x = triangle_x - 2
-      rect_y = triangle_y - 10
-      
-      Rectangle(pos=[rect_x, rect_y], size=[4, 6])
-      
-      # Redraw the line coloured
-      other_label = self.ids['pb%s' % self.swaps[char]]
-      Line(points=[label.center_x, label.center_y + 7,
-                   other_label.center_x, other_label.center_y - 7])
+    self.char_out = char_out
 
 class EmulatorGui(BoxLayout):
   
